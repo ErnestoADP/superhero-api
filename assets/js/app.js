b5d1a132d654d6e7bd311caacd68e8d2f188b374
Ernesto Diaz <ernesto.adp1@gmail.com>
2021-11-27 20:01:03 -0300

    quinto commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,77 @@
-window.onload = function() {
+/** ---------------------------   error: (error) => {
+        console.log(error `Codigo de rror: ${error.status}`)
+        console.log(error.statusText)
+        console.log(error.responseText)
+    }----------------------------------------------------------------------- */
 
+
+
+/**funcion para traer la api y parciarla mediante el metodo click por el boton traemos el numero*/
+$('#button-addon2').click(function(event) {
+        /**aqui parciamos y vemos la entrada del numero en el input */
+        let numeroHero = parseInt($('#inputNumber').val());
+
+        $.ajax({
+            /**aqui traemos la api mas el numero ingresado del input */
+            url: `https://superheroapi.com/api/10227257603166405/${numeroHero}`,
+            success: (datosHero) => {
+                generarGrafico(datosHero);
+                cuerpoCard(datosHero);
+                window.onload(datosHero);
+
+            },
+        });
+
+    })
+    /**--------------------------------------------------------------------------------------------*/
+
+/**funcion para traer los datos que necesitamos */
+function cuerpoCard(datosHero) {
+    tituloHeroe = $('.h1')
+    tituloHeroe.empty();
+    tituloHeroe.append("SuperHero Encontrado ");
+
+    nombreHeroe = $('.card-title')
+    nombreHeroe.empty();
+    nombreHeroe.append("Nombre: " + datosHero.name);
+
+
+
+    conexionHeroe = $('.card-text2')
+    conexionHeroe.empty();
+    conexionHeroe.append("Conexiones:  " + datosHero.connections["group-affiliation"]);
+
+    pHeroe = $('.card-text3')
+    pHeroe.empty();
+    pHeroe.append("Publicado por:  " + datosHero.biography.publisher);
+
+
+
+    workHeroe = $('.card-text1')
+    workHeroe.empty();
+    workHeroe.append("Ocupación:  " + datosHero.work.occupation);
+
+    primeraAparicionHeroe = $('.card-text4')
+    primeraAparicionHeroe.empty();
+    primeraAparicionHeroe.append("Primera aparición:  " + datosHero.biography["first-appearance"]);
+
+    alturaHeroe = $('.card-text5')
+    alturaHeroe.empty();
+    alturaHeroe.append("Altura:  " + datosHero.appearance.height);
+
+    pesoHeroe = $('.card-text6')
+    pesoHeroe.empty();
+    pesoHeroe.append("Peso:  " + datosHero.appearance.weight);
+
+    alianzasHeroe = $('.card-text7')
+    alianzasHeroe.empty();
+    alianzasHeroe.append("Alianzas:  " + datosHero.biography.aliases);
+
+    imgHero = $('.img-fluid').attr("src", datosHero.image.url);
+}
+/**--------------------------------------------------------------------------------------------*/
+/**grafico */
+function generarGrafico(datosHero) {
     let options = {
         title: {
             text: "Estadisticas de Poder para"
@@ -14,48 +86,20 @@ window.onload = function() {
             indexLabelFontSize: 16,
             indexLabel: "{label} - {y}%",
             dataPoints: [
-                { y: 69, label: "inteligence" },
-                { y: 100, label: "Strength" },
-                { y: 50, label: "Sepeed" },
-                { y: 100, label: "Durability" },
-                { y: 100, label: "Power" },
-                { y: 100, label: "Combat" },
+                { y: datosHero.powerstats.combat, label: "Combat" },
+                { y: datosHero.powerstats.intelligence, label: "inteligence" },
+                { y: datosHero.powerstats.strength, label: "Strength" },
+                { y: datosHero.powerstats.speed, label: "Sepeed" },
+                { y: datosHero.powerstats.durability, label: "Durability" },
+                { y: datosHero.powerstats.power, label: "Power" },
+
 
             ]
         }]
     };
     $("#chartContainer").CanvasJSChart(options);
 
-
-
 }
 
-$(document).ready(function() {
-    $('#button-addon2').on('click', () => {
-        $('.section').toggle(4000);
-    });
 
-});
-/**--------------------------------------------------------------------------------------------------- */
-let mostrar = $('#mostrar');
-$.ajax({
-    type: 'GET',
-    url: 'https://www.mindicador.cl/api',
-    success: (datosHero) => {
-        console.table(datosHero);
-        console.info(datosHero.dolar);
-
-        mostrar.html(`<h2>Dolar</h2>
-             <p>${datosHero.dolar.valor}</p>
-        
-        `);
-    },
-    error: (error) => {
-        console.log(error `Cdigo de rror: ${error.status}`)
-        console.log(error.statusText)
-        console.log(error.responseText)
-
-
-    }
-
-})
+/**--------------------------------------------------------------------------------------------*/
